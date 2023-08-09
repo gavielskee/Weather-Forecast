0309a938cbc0105b63954a7b6fd6803cf9fb6a8a
gavielskee <gavielskee1@gmail.com>
2023-08-09 10:38:42 -0400

    created API variables

---
+++ assets/javascript/script.js
@@ -0,0 +1,4 @@
+var WeatherAPI = "f4633e2e21e30b07d0578aa2be38d831";
+var queryURL: "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + f4633e2e21e30b07d0578aa2be38d831;
+var city;
+fetch(queryURL)
