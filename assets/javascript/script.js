24f9e0b47e12f1c381e07c679f64ec60f930549d
gavielskee <gavielskee1@gmail.com>
2023-08-10 18:09:02 -0400

    setting up basic functionality for pull[Ding current data

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,4 +1,22 @@
-var WeatherAPI = "f4633e2e21e30b07d0578aa2be38d831";
-var queryURL: "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + f4633e2e21e30b07d0578aa2be38d831;
-var city;
-fetch(queryURL)
+// Weather API Key
+var key = "f4633e2e21e30b07d0578aa2be38d831";
+var input = document.querySelector(".input")
+var btn = document.querySelector(".btn")
+
+
+btn.addEventListener("click", function() {
+    var city = input.value
+    fetchCurrent(city)
+})
+
+// var queryURL: "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + f4633e2e21e30b07d0578aa2be38d831;
+
+function fetchCurrent(cityName) {
+    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + key)
+    .then(function(res) {
+        return res.json()
+    })
+    .then(function(data) {
+        console.log(data)
+    })
+}
