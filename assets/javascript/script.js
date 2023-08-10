9c309c169ed605e3eb2c618faf9b64fd6933b091
gavielskee <gavielskee1@gmail.com>
2023-08-10 19:03:26 -0400

    created for loop to display forecast for cards

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -8,10 +8,17 @@ var temp = document.querySelector(".temp")
 var wind = document.querySelector(".wind")
 var humidity = document.querySelector(".humidity")
 
+var forecastCard = document.querySelectorAll(".forecastCard")
+
+
 btn.addEventListener("click", function () {
+    var history = JSON.parse(localStorage.getItem("cityHistory")) || []
     var city = input.value
     fetchCurrent(city)
     fetchForecast(city)
+    history.push(city)
+    localStorage.setItem("cityHistory", JSON.stringify(history))
+    renderHistory()
 })
 
 function fetchCurrent(cityName) {
@@ -41,6 +48,33 @@ function fetchForecast(cityName) {
         })
         .then(function (data) {
             console.log(data)
+            for(let i = 0; i < forecastCard.length; i++) {
+                forecastCard[i].innerHTML = ""
+                var index = i * 8 + 4
+                var currentDate = new Date(data.list[index].dt * 1000)
+                var forecastMonth = currentDate.getMonth() + 1
+                var forecastDay = currentDate.getDate()
+                var forecastYear = currentDate.getFullYear()
+                var dateEl = document.createElement("h4")
+                var icon = document.createElement("img")
+                var tempEl = document.createElement("p")
+                var windEl = document.createElement("p")
+                var humidityEl = document.createElement("p")
+                dateEl.innerHTML = "(" + forecastMonth + "/" + forecastDay + "/" + forecastYear + ")"
+                icon.setAttribute("src", "https://openweathermap.org/img/wn/" + data.list[index].weather[0].icon + "@2x.png")
+                icon.setAttribute("alt",  data.list[index].weather[0].description)
+                tempEl.innerHTML = "Temp: " + data.list[index].main.temp + "&#176F"
+                windEl.innerHTML = "Wind: " + data.list[index].wind.speed + " MPH"
+                humidityEl.innerHTML = "Humidity: " + data.list[index].main.humidity + "%"
+                forecastCard[i].append(icon)
+                forecastCard[i].append(tempEl)
+                forecastCard[i].append(dateEl)
+                forecastCard[i].append(windEl)
+                forecastCard[i].append(humidityEl)
+            }   
+        })  
+}
 
-        })
+function renderHistory() {
+    
 }
