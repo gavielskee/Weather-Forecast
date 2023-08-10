41013858aeee3d13e7cfd69eeaa1ce62e592dbeb
gavielskee <gavielskee1@gmail.com>
2023-08-10 18:27:51 -0400

    set up current weather data to render to the DOM

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,7 +2,11 @@
 var key = "f4633e2e21e30b07d0578aa2be38d831";
 var input = document.querySelector(".input")
 var btn = document.querySelector(".btn")
-
+var cityInfo = document.querySelector(".cityInfo")
+var icon = document.querySelector(".icon")
+var temp = document.querySelector(".temp")
+var wind = document.querySelector(".wind")
+var humidity = document.querySelector(".humidity")
 
 btn.addEventListener("click", function () {
     var city = input.value
@@ -17,6 +21,16 @@ function fetchCurrent(cityName) {
         })
         .then(function (data) {
             console.log(data)
+            var currentDate = new Date()
+            var month = currentDate.getMonth() + 1
+            var day = currentDate.getDate()
+            var year = currentDate.getFullYear()
+            cityInfo.innerHTML = data.name + " " + "(" + month + "/" + day + "/" + year + ")"
+            icon.setAttribute("src", "https://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png")
+            icon.setAttribute("alt",  data.weather[0].description)
+            temp.innerHTML = "Temp: " + data.main.temp + "&#176F"
+            wind.innerHTML = "Wind: " + data.wind.speed + " MPH"
+            humidity.innerHTML = "Humidity: " + data.main.humidity + "%"
         })
 }
 
@@ -27,5 +41,6 @@ function fetchForecast(cityName) {
         })
         .then(function (data) {
             console.log(data)
+
         })
 }
